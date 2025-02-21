92bdacad21c21f2959f7bd98cfa43519bd1167d9
Bence Kovacs <bencekovacs.ev@gmail.com>
2025-02-21 19:06:21 +0100

    Add unit tests, fix enum bug

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,7 +1,37 @@
 import unittest
 
 from textnode import TextNode, TextType
-
+        
 class TestTextNode(unittest.TestCase):
-    def test_eq(self):
-        node = TextNode("This is a text node", TextType.BOLD_TEXT)
+        def test_eq(self):
+            node = TextNode("This is a text node", TextType.BOLD)
+            node2 = TextNode("This is a text node", TextType.BOLD)
+            self.assertEqual(node, node2)
+            
+        def test_urlisnone(self):
+            node = TextNode("URL is None", TextType.NORMAL)
+            self.assertIsNone(node.url)
+            
+        def test_noteq(self):
+            node = TextNode("This is a text node", TextType.BOLD)
+            node2 = TextNode("This is a text node", TextType.NORMAL)
+            self.assertNotEqual(node, node2)
+            
+        def test_repr(self):
+            node = TextNode("This is a text node", TextType.ITALIC, "http://example.com")
+            self.assertEqual(repr(node), "TextNode(This is a text node, italic, http://example.com)")
+            
+        def test_text_type(self):
+            node = TextNode("This is a text node", TextType.CODE)
+            self.assertEqual(node.text_type, TextType.CODE)
+            
+        def test_text(self):
+            node = TextNode("This is a text node", TextType.LINK, "http://example.com")
+            self.assertEqual(node.text, "This is a text node")
+            
+        def test_url(self):
+            node = TextNode("This is a text node", TextType.IMAGE, "http://example.com/image.png")
+            self.assertEqual(node.url, "http://example.com/image.png")
+        
+if __name__ == "__main__":
+    unittest.main()
--- src/textnode.py
+++ src/textnode.py
@@ -1,11 +1,11 @@
 from enum import Enum
 
 class TextType(Enum):
-    NORMAL_TEXT = "normal text"
-    BOLD_TEXT = "bold text",
-    ITALIC_TEXT = "italic text",
-    CODE_TEXT = "code text",
-    LINK = "link",
+    NORMAL= "normal"
+    BOLD = "bold"
+    ITALIC = "italic"
+    CODE = "code"
+    LINK = "link"
     IMAGE = "image"
     
 class TextNode:
@@ -14,7 +14,7 @@ class TextNode:
         self.text_type = text_type
         self.url = url
         
-    def __eg__(self, other):
+    def __eq__(self, other):
         return self.text == other.text and self.text_type == other.text_type and self.url == other.url
       
     def __repr__(self):
